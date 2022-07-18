c68c20beb7f86b2a414d14b72f2e8fa46a414f00
vishve <41191728+vishve@users.noreply.github.com>
2022-07-18 06:44:27 +0530

    added index2 and js2 files on HTML DOM

---
+++ JS_index_2.js
@@ -0,0 +1,53 @@
+//Source: https://youtu.be/5fb2aPlgoys
+//DOM Manupulation
+// //getElementById 
+// const title = document.getElementById('main-heading');
+// console.log(title)
+
+// //getElementsByTagName
+// const Lists = document.getElementsByTagName('li');
+// console.log(lists);
+// //getElementsByClassName
+// const list_items = document.getElementsByClassName('list-items');
+// console.log(list_items);
+
+// //querySelector
+// const containerr = document.querySelector ('div');
+// console.log (containerr);
+
+// //querySelectorAll
+// const containers = document.querySelectorAll('div');
+// const headinger = document.querySelectorAll(" #main-heading");
+
+// console.log (containers);
+
+// approximately all intro  html DOM is over.
+
+// going back to the file.and commenting out the second sports container div block 
+//and commenting out all the above examples for document model.
+// now lets do the HTML Styling (CSS)
+
+
+//getElementById 
+const title = document.getElementById('main-heading');
+console.log(title)
+// inline styling
+title.style.color = 'red';
+
+//getElementsByTagName
+const Lists = document.getElementsByTagName('li');
+console.log(lists);
+//getElementsByClassName
+const list_items = document.getElementsByClassName('list-items');
+console.log(list_items);
+
+//querySelector
+const containerr = document.querySelector ('div');
+console.log (containerr);
+
+//querySelectorAll
+const containers = document.querySelectorAll('list-items');
+
+console.log (containers);
+
+
