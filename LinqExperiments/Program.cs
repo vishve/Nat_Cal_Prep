e5f4daa4183956e46c045f2fb8917f596160841d
vishve <41191728+vishve@users.noreply.github.com>
2023-01-03 10:46:13 +0530

    updated the csproj and project files for old net5 coding so that main and class can be controlled.

--- LinqExperiments/Program.cs
+++ LinqExperiments/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!"); 
+// Changes done as below and in the csproj file with <ImplicitUsings>disable</ImplicitUsings>
+// brings me back to the pre .net 6 coding status, now I can include and exclude whatevver I want.
+
+
+using System;
+
+namespace LinqExperiments // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
